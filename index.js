665dde451ea9e9685211166a425c0929a5d4cabb
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-11-14 21:48:31 +0000

    Added error handling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default;
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -5,31 +6,34 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-const axios = require('axios').default;
 const { PORT = 3000, API_KEY } = process.env;
 const url = (query) => (`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', { giphys:[] });
+  res.render('index', { giphys:[], error: null });
 });
 
 app.post('/search', async (req, res) => {
-  const { search } = req.body;
-  const giphys = await getGiphys(search);
-  res.render('index.ejs', { giphys });
+  try {
+    const { search } = req.body;
+    const giphys = await getGiphys(search);
+    res.render('index', { giphys, error: null });
+  } catch (error) {
+    res.render('index', { giphys:[], error });
+  }
 });
 
 const getGiphys = async (query) => {
   const fullUrl = url(query);
-  const response = await axios.get(fullUrl); 
+  const response = await axios.get(fullUrl);
   return response.data.data.map(item => {
     const { url, images } = item;
     return `
-    <p>
-      <a href="${url}" target="_blank">
-        <img src="${images.fixed_width.url}}" />
-      </a>
-    </p>`;
+      <p>
+        <a href="${url}" target="_blank">
+          <img src="${images.fixed_width.url}}" />
+        </a>
+      </p>`;
   }).join('');
 }
 
